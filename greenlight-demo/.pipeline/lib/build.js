b7d097893fa7b5444371a7f047ce0bcb495f9b48
Emiliano Suñé <emiliano.sune@gmail.com>
2019-09-06 16:12:28 -0700

    Correct labeling of build artifacts
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

--- greenlight-demo/.pipeline/lib/build.js
+++ greenlight-demo/.pipeline/lib/build.js
@@ -1,69 +1,80 @@
-'use strict';
-const {
-  OpenShiftClientX
-} = require('pipeline-cli')
-const path = require('path');
+"use strict";
+const { OpenShiftClientX } = require("pipeline-cli");
+const path = require("path");
 
-module.exports = (settings) => {
-  const phases = settings.phases
-  const options = settings.options
-  const oc = new OpenShiftClientX(Object.assign({
-    'namespace': phases.build.namespace
-  }, options));
-  const phase = 'build'
-  let objects = []
-  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../../openshift/'))
+module.exports = settings => {
+  const phases = settings.phases;
+  const options = settings.options;
+  const oc = new OpenShiftClientX(
+    Object.assign(
+      {
+        namespace: phases.build.namespace
+      },
+      options
+    )
+  );
+  const phase = "build";
+  let objects = [];
+  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../../openshift"));
 
   // caddy runtime build
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/caddy-runtime/caddy-runtime-build.json`, {
-    'param': {
-      'NAME': 'caddy-runtime' + phases[phase].suffix,
-      'GIT_REPO_URL': oc.git.http_url,
-      'GIT_REF': oc.git.ref,
-      'SOURCE_CONTEXT_DIR': '',
-      'OUTPUT_IMAGE_TAG': 'latest',
-      'SOURCE_IMAGE_KIND': 'ImageStreamTag',
-      'SOURCE_IMAGE_NAME': 'bcgov-s2i-caddy',
-      'SOURCE_IMAGE_NAMESPACE': 'openshift',
-      'SOURCE_IMAGE_TAG': 'latest',
-      'DOCKER_FILE_PATH': 'docker/proxy/Dockerfile'
-    }
-  }));
+  objects.push(
+    ...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/caddy-runtime/caddy-runtime-build.json`, {
+      param: {
+        NAME: "caddy-runtime",
+        SUFFIX: phases[phase].suffix,
+        VERSION: phases[phase].tag,
+        GIT_REPO_URL: oc.git.http_url,
+        GIT_REF: oc.git.ref,
+        SOURCE_CONTEXT_DIR: "",
+        SOURCE_IMAGE_KIND: "ImageStreamTag",
+        SOURCE_IMAGE_NAME: "bcgov-s2i-caddy",
+        SOURCE_IMAGE_NAMESPACE: "openshift",
+        SOURCE_IMAGE_TAG: "latest",
+        DOCKER_FILE_PATH: "docker/proxy/Dockerfile"
+      }
+    })
+  );
 
   // angular app build
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/greenlight-angular/greenlight-angular-build.json`, {
-    'param': {
-      'NAME': 'greenlight-angular' + phases[phase].suffix,
-      'GIT_REPO_URL': oc.git.http_url,
-      'GIT_REF': oc.git.ref,
-      'OUTPUT_IMAGE_TAG': 'latest',
-      'SOURCE_CONTEXT_DIR': '',
-      'DOCKER_FILE_PATH': 'docker/angular-build/Dockerfile',
-      'CPU_LIMIT': '1',
-      'MEMORY_LIMIT': '4Gi',
-      'CPU_REQUEST': '250m',
-      'MEMORY_REQUEST': '2Gi'
-    }
-  }));
+  objects.push(
+    ...oc.processDeploymentTemplate(
+      `${templatesLocalBaseUrl}/templates/greenlight-angular/greenlight-angular-build.json`,
+      {
+        param: {
+          NAME: "greenlight-angular",
+          SUFFIX: phases[phase].suffix,
+          VERSION: phases[phase].tag,
+          GIT_REPO_URL: oc.git.http_url,
+          GIT_REF: oc.git.ref,
+          SOURCE_CONTEXT_DIR: "",
+          DOCKER_FILE_PATH: "docker/angular-build/Dockerfile",
+          CPU_LIMIT: "1",
+          MEMORY_LIMIT: "4Gi",
+          CPU_REQUEST: "250m",
+          MEMORY_REQUEST: "2Gi"
+        }
+      }
+    )
+  );
 
   // final angular on caddy
-  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/greenlight/greenlight-build.json`, {
-    'param': {
-      'NAME': phases[phase].name + phases[phase].suffix,
-      'SOURCE_IMAGE_NAMESPACE': '',
-      'SOURCE_IMAGE_NAME': 'greenlight-angular',
-      'SOURCE_IMAGE_TAG': 'latest',
-      'RUNTIME_IMAGE_NAMESPACE': 'devex-bcgov-dap-tools',
-      'RUNTIME_IMAGE_NAME': 'caddy-runtime',
-      'RUNTIME_IMAGE_TAG': 'latest',
-      'OUTPUT_IMAGE_TAG': 'latest'
-      // 'SUFFIX': phases[phase].suffix,
-      // 'VERSION': phases[phase].tag,
-      // 'SOURCE_REPOSITORY_URL': oc.git.http_url,
-      // 'SOURCE_REPOSITORY_REF': oc.git.ref
-    }
-  }));
+  objects.push(
+    ...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/greenlight/greenlight-build.json`, {
+      param: {
+        NAME: phases[phase].name,
+        SUFFIX: phases[phase].suffix,
+        VERSION: phases[phase].tag,
+        SOURCE_IMAGE_NAMESPACE: "",
+        SOURCE_IMAGE_NAME: "greenlight-angular",
+        SOURCE_IMAGE_TAG: phases[phase].tag,
+        RUNTIME_IMAGE_NAMESPACE: "devex-bcgov-dap-tools",
+        RUNTIME_IMAGE_NAME: "caddy-runtime",
+        RUNTIME_IMAGE_TAG: phases[phase].tag
+      }
+    })
+  );
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
-  oc.applyAndBuild(objects)
-}
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance);
+  oc.applyAndBuild(objects);
+};
--- greenlight-demo/.pipeline/lib/deploy.js
+++ greenlight-demo/.pipeline/lib/deploy.js
@@ -1,19 +1,58 @@
-'use strict';
-const {OpenShiftClientX} = require('pipeline-cli')
-const path = require('path');
+"use strict";
+const { OpenShiftClientX } = require("pipeline-cli");
+const path = require("path");
 
-module.exports = (settings)=>{
-  const phases = settings.phases
-  const options= settings.options
-  const phase=options.env
-  const changeId = phases[phase].changeId
-  const oc=new OpenShiftClientX(Object.assign({'namespace':phases[phase].namespace}, options));
-  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, '../../openshift'))
-  var objects = []
+module.exports = settings => {
+  const phases = settings.phases;
+  const options = settings.options;
+  const phase = options.env;
+  const changeId = phases[phase].changeId;
+  const oc = new OpenShiftClientX(Object.assign({ namespace: phases[phase].namespace }, options));
+  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, "../../../openshift"));
+  var objects = [];
 
-  // The deployment of your cool app goes here ▼▼▼
+  objects.push(
+    ...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/templates/greenlight/greenlight-deploy.json`, {
+      param: {
+        NAME: phases[phase].name,
+        SUFFIX: phases[phase].suffix,
+        VERSION: phases[phase].tag,
+        APPLICATION_DOMAIN: `${phases[phase].name}${phases[phase].suffix}-${phases[phase].namespace}.pathfinder.gov.bc.ca`,
+        APP_GROUP: phases[phase].phase,
+        IMAGE_NAMESPACE: "devex-bcgov-dap-tools",
+        TAG_NAME: phases[phase].tag,
+        TOB_API_URL: "https://dev-demo.orgbook.gov.bc.ca/api",
+        TOB_APP_URL: "https://dev-demo.orgbook.gov.bc.ca",
+        WEB_HOST_NAME: "0.0.0.0",
+        WEB_HOST_PORT: "8080",
+        WEB_HOST_TLS: "off",
+        WEB_BASE_HREF: "/",
+        MINISTRY_AGRICULTURE_AGENT_HOST: "ministry-of-agriculture" + phases[phase].suffix,
+        MINISTRY_AGRICULTURE_AGENT_PORT: "8080",
+        BCREG_AGENT_HOST: "bc-registries" + phases[phase].suffix,
+        BCREG_AGENT_PORT: "8080",
+        MINISTRY_FINANCE_AGENT_HOST: "ministry-of-finance" + phases[phase].suffix,
+        MINISTRY_FINANCE_AGENT_PORT: "8080",
+        CITY_SURREY_AGENT_HOST: "city-of-surrey" + phases[phase].suffix,
+        CITY_SURREY_AGENT_PORT: "8080",
+        FRASER_VALLEY_AGENT_HOST: "fraser-valley-health-authority" + phases[phase].suffix,
+        FRASER_VALLEY_AGENT_PORT: "8080",
+        LIQUOR_CONTROL_AGENT_HOST: "liquor-control-and-licensing-branch" + phases[phase].suffix,
+        LIQUOR_CONTROL_AGENT_PORT: "8080",
+        WORKSAFE_AGENT_HOST: "worksafe-bc" + phases[phase].suffix,
+        WORKSAFE_AGENT_PORT: "8080",
+        CONFIG_FILE_NAME: "Caddyfile",
+        CONFIG_MAP_NAME: "caddy-conf",
+        CONFIG_MOUNT_PATH: "/etc/",
+        CPU_LIMIT: "250m",
+        MEMORY_LIMIT: "256Mi",
+        CPU_REQUEST: "10m",
+        MEMORY_REQUEST: "128Mi"
+      }
+    })
+  );
 
-  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance)
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag)
-  oc.applyAndDeploy(objects, phases[phase].instance)
-}
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, `${changeId}`, phases[phase].instance);
+  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
+  oc.applyAndDeploy(objects, phases[phase].instance);
+};
